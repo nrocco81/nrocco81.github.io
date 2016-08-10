3b7331df48b66b0aeed8115e99f73c6f9003592d
nrocco81 <nrocco@gmail.com>
2016-08-10 10:43:57 -0700

    Add files via upload

---
+++ projectController.js
@@ -0,0 +1,28 @@
+mainApp.controller('projectController', function($scope) {
+   $scope.project = {         //sets up default values
+      jobNumber: "[Job#]",
+      description: "[Description]",
+      value: 500,
+      month: 1,
+
+      wbs:[
+         {id:'1', month1:70, month2:150, month3:90},
+         {id:'1.1', month1:80, month2:50, month3:23},
+         {id:'1.2', month1:65, month2:200, month3:76},
+         {id:'2', month1:75, month2:75, month3:56},
+         {id:'2.1', month1:67, month2:85, month3:200}
+      ],
+
+      fullName: function() {    //combines two text fields to one
+         var projectObject = $scope.project;
+         return projectObject.jobNumber + ", " + projectObject.description;
+      }
+   };
+   $scope.reset = function(){   //when reset button pressed, sets default values
+     $scope.project.jobNumber = "[Job#]";
+     $scope.project.description = "[Description]";
+     $scope.project.value = 500;
+     $scope.project.month = 1;
+   }
+
+});
