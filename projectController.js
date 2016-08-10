3e3df3c38b87c15626287da98d1875a71bf8ffa6
nrocco81 <nrocco@gmail.com>
2016-08-10 11:50:06 -0700

    Update projectController.js

--- projectController.js
+++ projectController.js
@@ -1,10 +1,10 @@
 mainApp.controller('projectController', function($scope) {
+   
    $scope.project = {         //sets up default values
       jobNumber: "[Job#]",
       description: "[Description]",
       value: 500,
       month: 1,
-
       wbs:[
          {id:'1', month1:70, month2:150, month3:90},
          {id:'1.1', month1:80, month2:50, month3:23},
@@ -12,23 +12,23 @@ mainApp.controller('projectController', function($scope) {
          {id:'2', month1:75, month2:75, month3:56},
          {id:'2.1', month1:67, month2:85, month3:200}
       ],
-
       fullName: function() {    //combines two text fields to one
          var projectObject = $scope.project;
          return projectObject.jobNumber + ", " + projectObject.description;
       }
    };
+   
    $scope.reset = function(){   //when reset button pressed, sets default values
      $scope.project.jobNumber = "[Job#]";
      $scope.project.description = "[Description]";
      $scope.project.value = 500;
      $scope.project.month = 1;
    }
+   
    $scope.student = {
       firstName: "Nick",
       lastName: "Rocco",
       fees:500,
-      
       subjects:[
          {name:'Physics',marks:70},
          {name:'Chemistry',marks:80},
@@ -36,11 +36,11 @@ mainApp.controller('projectController', function($scope) {
          {name:'English',marks:75},
          {name:'Hindi',marks:67}
       ],
-      
       fullName: function() {
          var studentObject;
          studentObject = $scope.student;
          return studentObject.firstName + " " + studentObject.lastName;
       }
    };
+   
 });
